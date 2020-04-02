0b7edb7480cdb8e66c5bf153162e24d0502f6396
zhouxishun <1050494995@qq.com>
2020-04-02 14:35:56 +0800

    页面搭建

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,13 @@ module.exports = {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
   },
+  rules: {
+    'jsx-a11y/label-has-associated-control': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-console': 'off',
+    'jsx-a11y/anchor-has-content': 'off',
+    'no-restricted-syntax': 'off',
+    'no-bitwise': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
 };
